ff5132944c06467fefa3359878cac1dbd7bc2626
OleguerCanal <oleguer.canal@hotmail.com>
2022-05-06 21:09:28 +0200

    scratch training working

--- test_utils/models.py
+++ test_utils/models.py
@@ -2,6 +2,8 @@ import torch
 
 # from attentions.attentions import MultiHeadAttention
 
+
+## LinearNet
 class LinearBlock(torch.nn.Module):
     def __init__(self,
                  in_features : int,
@@ -16,18 +18,6 @@ class LinearBlock(torch.nn.Module):
         x = self.activation(self.layer_1(x))
         # return self.activation(self.layer_2(x))
         return x
-
-class ConvBlock(torch.nn.Module):
-    def __init__(self, 
-                 in_channels : int,
-                 out_channels : int,
-                 kernel_size : int) -> None:
-        self.conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size)
-        self.pool = torch.nn.MaxPool2d(kernel_size=2)
-        self.activation = torch.nn.ReLU()
-    
-    def forward(self, x : torch.Tensor) -> torch.Tensor:
-        x = self.pool(self.activation(self.conv(x)))
 
 class LinearNet(torch.nn.Module):
     def __init__(self,
@@ -48,21 +38,40 @@ class LinearNet(torch.nn.Module):
             x = layer(x)
         return self.out_layer(x)
 
+
+## ConvNet
+class ConvBlock(torch.nn.Module):
+    def __init__(self, 
+                 in_channels : int,
+                 out_channels : int,
+                 kernel_size : int = 3) -> None:
+        super().__init__()
+        self.conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2)
+        self.activation = torch.nn.ReLU()
+    
+    def forward(self, x : torch.Tensor) -> torch.Tensor:
+        return self.pool(self.activation(self.conv(x)))
+
+
 class ConvNet(torch.nn.Module):
     def __init__(self,
                  in_features : int,
+                 hidden_dim: int,
                  out_features : int,
                  n_blocks : int) -> None:
         super().__init__()
 
-        self.hidden_layers = torch.nn.ModuleList(
-            modules = [LinearBlock(in_features, in_features) for _ in range(n_blocks)]
-        )
-        self.out_layer = torch.nn.Linear(in_features, out_features)
-
+        modules = [ConvBlock(in_features, hidden_dim)]
+        modules += [ConvBlock(hidden_dim, hidden_dim) for _ in range(n_blocks-1)]
+        self.hidden_layers = torch.nn.ModuleList(modules = modules)
+        self.out_layer = torch.nn.Linear(256, out_features)
 
     def forward(self,
                 x: torch.Tensor) -> torch.Tensor:
+        """ Input: (B,C,H,W)
+        """
         for layer in self.hidden_layers:
             x = layer(x)
+        x = x.view(x.size(0), -1)
         return self.out_layer(x)
--- test_utils/scratch_training.py
+++ test_utils/scratch_training.py
@@ -1,10 +1,13 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from tqdm import tqdm
 
 import torch
 import torchvision
 import torchvision.transforms as transforms
+
+from models import ConvNet
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -22,17 +25,17 @@ testset = torchvision.datasets.CIFAR10(root='./data', train=False,
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                          shuffle=False, num_workers=2)
 
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+classes = ('plane', 'car', 'bird', 'cat','deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
 
+net = ConvNet(3, 64, 10, n_blocks=3)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 
 for epoch in range(2):  # loop over the dataset multiple times
-
     running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
+    for i, data in tqdm(enumerate(trainloader, 0)):
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
 
@@ -46,9 +49,7 @@ for epoch in range(2):  # loop over the dataset multiple times
         optimizer.step()
 
         # print statistics
-        running_loss += loss.item()
         if i % 2000 == 1999:    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-            running_loss = 0.0
+            print("loss:", loss.item())
 
 print('Finished Training')
